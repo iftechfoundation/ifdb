0f1a4b623c45ce4329dfe60a836700637eda2aa2
Iniquit <iniquitwrites@gmail.com>
2021-03-21 11:03:15 -0400

    Break check-inbox.php out of home.php
    This is the first of several commits that moves a chunk of display logic out of the main home.php file into a subdirectory provisionally labelled "components" and then uses a php include to reference it in home.php.
    
    The intent is to make home.php easier to work with in future commits by reducing its length and complexity.

---
+++ www/components/check-inbox.php
@@ -0,0 +1,38 @@
+<?php
+// -------------------------- inbox ----------------------------
+
+// check the inbox
+if ($quid) {
+    list($inbox, $inboxCnt) =
+        queryComments($db, "inbox", $quid, "limit 0, 1", $caughtUpDate, false);
+    if (!$newCaughtUp || $srow[4] > $newCaughtUp)
+        $newCaughtUp = $srow[4];
+
+    if ($inboxCnt) {
+
+        echo "<div class=headline style=\"clear:right;\">"
+            . "<span class=headlineRss>"
+            . "<a href=\"commentlog?user=$quid&mode=inbox&rss\">"
+            . "<img src=\"blank.gif\" class=\"rss-icon\">Your Inbox (RSS)</a>"
+            . "</span>"
+            . "Your Discussions</div>";
+
+        $new = $since = "";
+        if ($caughtUpDate) {
+            $new = " new";
+            $since = "since ". date("F j, Y", strtotime($caughtUpDate));
+        }
+        
+        list($crow) = $inbox[0];
+        $cid = $crow[0];
+        $cdate = $crow[5];
+        $newCaughtUp = $crow[4];
+        echo "You have $inboxCnt $new item" . ($inboxCnt == 1 ? "" : "s")
+            . " $since in your <a href=\"commentlog?mode=inbox\">"
+            . "comment inbox</a>"
+            . ($caughtUpDate ? "" : " (latest on $cdate)")
+            . ".<br>";
+    }
+}
+
+         ?>
