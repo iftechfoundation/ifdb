fdc7073388dec0ce69b2370533ca40717b8c53df
Iniquit <iniquitwrites@gmail.com>
2021-03-23 16:31:06 -0400

    Add semantic <ol> and <li> elements to top-reviewers widget
    (And then add styles to preserve its current appearance)

--- www/components/top-reviewers.php
+++ www/components/top-reviewers.php
@@ -1,19 +1,20 @@
-<div class="rightbar">
+<div id="top-reviewers-widget" class="rightbar">
    <h3>Reviewer Trophy Room</h3>
    <span class=details><i>IFDB's top reviewers, as determined by <?php echo helpWinLink("help-ff", "Frequent Fiction")?> scores:</i></span><p>
 
+   <ol>
    <?php
    $rlst = getTopReviewers($db, 4);
    $n = 1;
    foreach ($rlst as $r) {
       list($ruid, $runame, $rscore) = $r;
       $runame = htmlspecialcharx($runame);
-      echo "<span style='padding: 0 1.5ex 0 1ex;' class=details><i>#$n</i></span>"
-         . "<a class=silent href=\"showuser?id=$ruid\">$runame</a>"
-         . "<br>";
+      echo "<li><span style='padding: 0 1.5ex 0 1ex;' class=details><i>#$n</i></span>"
+         . "<a class=silent href=\"showuser?id=$ruid\">$runame</a></li>";
       $n++;
    }
    ?>
+   </ol>
    
    <p><span class=details>
          <a href="search?sortby=ffrank&browse=1&member=1">See all</a> | 
