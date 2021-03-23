2c3df3e0ad0b4fb823a8cec1799c8bd37310e529
Iniquit <iniquitwrites@gmail.com>
2021-03-23 14:57:01 -0400

    Space & indent top-reviewers widget

--- www/components/top-reviewers.php
+++ www/components/top-reviewers.php
@@ -1,37 +1,22 @@
-<?php
-   // ------------------------ top reviewers insert --------------------
-    ?>
+<div class="rightbar">
+   <h3>Reviewer Trophy Room</h3>
+   <span class=details><i>IFDB's top reviewers, as determined by <?php echo helpWinLink("help-ff", "Frequent Fiction")?> scores:</i></span><p>
 
-
-         <div class="rightbar">
-               <h3>Reviewer Trophy Room</h3>
-                  <span class=details><i>IFDB's top reviewers, as determined
-                     by <?php echo helpWinLink("help-ff", "Frequent Fiction")
-                     ?> scores:</i></span><p>
-                  <?php
-
-$rlst = getTopReviewers($db, 4);
-$n = 1;
-foreach ($rlst as $r) {
-    list($ruid, $runame, $rscore) = $r;
-    $runame = htmlspecialcharx($runame);
-    echo "<span style='padding: 0 1.5ex 0 1ex;' class=details><i>#$n</i></span>"
-        . "<a class=silent href=\"showuser?id=$ruid\">$runame</a>"
-        . "<br>";
-    $n++;
-}
-
-                  ?>
-
-                  <p><span class=details>
-                     <a href="search?sortby=ffrank&browse=1&member=1">
-                     See all</a> |
-                  <?php echo helpWinLink(
-                      "help-top-rev", "Who qualifies?") ?>
-                  </span>
-                  </p>
-         </div>
-
-<?php
-// -------------------- end reviewers insert ------------------------------
-?>
+   <?php
+   $rlst = getTopReviewers($db, 4);
+   $n = 1;
+   foreach ($rlst as $r) {
+      list($ruid, $runame, $rscore) = $r;
+      $runame = htmlspecialcharx($runame);
+      echo "<span style='padding: 0 1.5ex 0 1ex;' class=details><i>#$n</i></span>"
+         . "<a class=silent href=\"showuser?id=$ruid\">$runame</a>"
+         . "<br>";
+      $n++;
+   }
+   ?>
+   
+   <p><span class=details>
+         <a href="search?sortby=ffrank&browse=1&member=1">See all</a> | 
+         <?php echo helpWinLink("help-top-rev", "Who qualifies?") ?>
+      </span>
+</div>
