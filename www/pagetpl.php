a0671dbcedfaae8b2278e5c60d68ce4d33a24272
Iniquit <iniquitwrites@gmail.com>
2021-04-07 11:48:34 -0400

    Add updated favicon
    I'm using https://realfavicongenerator.net to try to catch all the different contexts in which the favicon might appear. It automatically generates a number of extra versions to make the favicon play nice on mobile homescreens, new tab pages, etc.

--- www/pagetpl.php
+++ www/pagetpl.php
@@ -23,8 +23,16 @@ function basePageHeader($title, $focusCtl, $extraOnLoad, $extraHead,
 ?>
 <html>
 <head>
-   <link rel="icon" type="image/png" href="/ifdb-favicon.png">
-   <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#9a165a">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#603cba">
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    
    <link rel="search" type="application/opensearchdescription+xml"
          title="IFDB Search Plugin"
          href="<?php echo get_root_url() ?>plugins/ifdb-opensearchdesc.xml">
