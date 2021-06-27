34a22d9f818a50c1e96726c184f873188bd249a7
Dan Fabulich <dan.fabulich@redfin.com>
2021-06-26 17:58:22 -0700

    Update to June backup

--- prepare_dev_environment.sh
+++ prepare_dev_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
-# TODO: detect latest archive
-FILENAME=ifdb-archive-20210317.zip
+#We're not auto-detecting DB updates because if we do any schema changes in production, we'll have to remove them from patch-schema.sql when we download the updated archive, months later
+#FILENAME=`curl --silent https://ifarchive.org/if-archive/info/ifdb/ | grep --only-matching "ifdb-archive-\d*.zip" | tail -1`
+FILENAME=ifdb-archive-20210601.zip
 if [ ! -f sql/$FILENAME ]; then
     curl -o sql/$FILENAME https://ifarchive.org/if-archive/info/ifdb/$FILENAME
 fi
