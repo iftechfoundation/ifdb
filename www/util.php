b5632f56c7fefb1a0ecd6194fa716ccad66ec116
Dan Fabulich <dan@fabulich.com>
2021-12-20 21:49:42 -0800

    Make coverart no more than 35% of screen width

--- www/util.php
+++ www/util.php
@@ -3211,7 +3211,9 @@ function coverArtThumbnail($id, $size, $params = "") {
     $x15 = round($size * 3 / 2);
     $x2 = $size * 2;
     $x3 = $size * 3;
-    return "<img srcset=\"$thumbnail{$size}x$size$params, $thumbnail{$x15}x$x15$params 1.5x, $thumbnail{$x2}x$x2$params 2x, $thumbnail{$x3}x$x3$params 3x\" src=\"/viewgame?id=$id&coverart&thumbnail={$size}x$size\" height=$size width=$size border=0 alt=\"\">";
+    global $nonce;
+    return "<style nonce='$nonce'>.coverart__img { max-width: 35vw; height: auto; }</style>"
+        ."<img class='coverart__img' srcset=\"$thumbnail{$size}x$size$params, $thumbnail{$x15}x$x15$params 1.5x, $thumbnail{$x2}x$x2$params 2x, $thumbnail{$x3}x$x3$params 3x\" src=\"/viewgame?id=$id&coverart&thumbnail={$size}x$size\" height=$size width=$size border=0 alt=\"\">";
 }
 
 // ----------------------------------------------------------------------------
