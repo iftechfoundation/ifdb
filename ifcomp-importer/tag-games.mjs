f0e9cfc6df122be2108ea976d4a856c352730b00
Dan Fabulich <dan@fabulich.com>
2023-10-01 18:42:00 -0700

    tag IFComp games using new API

--- ifcomp-importer/tag-games.mjs
+++ ifcomp-importer/tag-games.mjs
@@ -3,26 +3,13 @@ import { readFile } from 'node:fs/promises';
 
 const games = JSON.parse(await readFile('microdata-tuids.json', 'utf8'));
 
-// TODO in the future, use the new taggame API that takes a username/password
-const login = await fetch(`${url}/login`, {
-    method: 'post',
-    body: new URLSearchParams({
-        userid: username,
-        password,
-    }),
-});
-
-const cookie = login.headers.getSetCookie()[0].split(';')[0];
-
 for (const {name, tuid} of games) {
     console.log(name, tuid);
     const result = await fetch(`${url}/taggame?xml`, {
         method: 'post',
-        headers: {
-            cookie
-        },
-        credentials: 'include',
         body: new URLSearchParams({
+            username,
+            password,
             id: tuid,
             t0: `IFComp ${compYear}`,
         }),
