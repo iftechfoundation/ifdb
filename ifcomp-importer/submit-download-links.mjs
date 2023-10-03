0bd06c1e8a48b69b4f3c714212d311cf968526cf
Dan Fabulich <dan@fabulich.com>
2023-10-02 17:06:23 -0700

    parallelize submit-download-links

--- ifcomp-importer/submit-download-links.mjs
+++ ifcomp-importer/submit-download-links.mjs
@@ -1,6 +1,7 @@
 import { username, password, url, compYear } from './settings.mjs';
 import { readFile } from 'fs/promises';
 import { XMLParser } from 'fast-xml-parser';
+import { runTasks } from './run-tasks.mjs';
 
 const games = JSON.parse(await readFile('microdata-downloads-tuids.json', 'utf8'));
 
@@ -27,63 +28,73 @@ function escapeXml(unsafe) {
     });
 }
 
+let tasks = [];
+
 for (const game of games) {
-    const { name, tuid, zipFileName, zipMainFile } = game;
+    tasks.push(async () => {
+        const { name, tuid, zipFileName, zipMainFile } = game;
+        if (!zipFileName) {
+            console.log(name, `${url}/viewgame?id=${tuid}`, "SKIPPED");
+            return;
+        }
 
-    const viewgameUrl = `${url}/viewgame?id=${tuid}&ifiction`;
-    let response;
-    try {
-        response = await fetch(viewgameUrl);
-    } catch (e) { }
-    if (!response?.ok) {
-        throw new Error(`Failed (${response?.status}) loading ${viewgameUrl}`);
-    }
-    const xml = new XMLParser().parse(await response.text());
+        const viewgameUrl = `${url}/viewgame?id=${tuid}&ifiction`;
+        let response;
+        try {
+            response = await fetch(viewgameUrl);
+        } catch (e) { }
+        if (!response?.ok) {
+            throw new Error(`Failed (${response?.status}) loading ${viewgameUrl}`);
+        }
+        const xml = new XMLParser().parse(await response.text());
 
-    const downloadLink = `https://ifarchive.org/if-archive/games/competition${compYear}/${escape(zipFileName)}`;
-    const downloadTitle = zipFileName.replace(/^Games\//, "");
-    let fileType = "";
-    if (zipMainFile) {
-        const extension = zipMainFile.split(".").pop();
-        fileType = fileTypes[extension];
-        if (!fileType) throw new Error("missing fileType for extension: " + extension);
-    } else {
-        fileType = "storyfile"
-    }
-    const body = new FormData();
-    body.append('username', username);
-    body.append('password', password);
-    body.append('lastversion', xml.ifindex.story.ifdb.pageversion);
-    const ifiction = `<?xml version="1.0" encoding="UTF-8"?>
-    <ifindex version="1.0" xmlns="http://babel.ifarchive.org/protocol/iFiction/">
-    <story>
-    <identification><tuid>${tuid}</tuid></identification>
-    </story>
-    </ifindex>
-    `
-    body.append('ifiction', new Blob([ifiction], { type: 'text/xml' }));
-    const links = `<?xml version="1.0" encoding="UTF-8"?>
-    <downloads xmlns="http://ifdb.org/api/xmlns"><links>
-    <link>
-    <url>${downloadLink}</url>
-    <title>${downloadTitle}</title>
-    <isGame/>
-    <format>${fileType}</format>
-    ${fileType === 'executable' ? `<os>Windows.</os>`: ''}
-    <compression>zip</compression>
-    ${zipMainFile ? `<compressedPrimary>${zipMainFile}</compressedPrimary>`: ''}
-    </link>
-    </links></downloads>
-    `;
-    body.append('links', new Blob([links], { type: 'text/xml' }));
-    response = await fetch(`${url}/putific`, {
-        method: 'post',
-        body,
-    });
-    const { status, statusText, ok } = response;
-    const text = await response.text();
-    if (!ok) {
-        throw new Error(`Failed ${status} ${statusText} adding download link for ${name}: ${text}`);
-    }
-    console.log(name, `${url}/viewgame?id=${tuid}`, "OK");
+        const downloadLink = `https://ifarchive.org/if-archive/games/competition${compYear}/${escape(zipFileName)}`;
+        const downloadTitle = zipFileName.replace(/^Games\//, "");
+        let fileType = "";
+        if (zipMainFile) {
+            const extension = zipMainFile.split(".").pop();
+            fileType = fileTypes[extension];
+            if (!fileType) throw new Error("missing fileType for extension: " + extension);
+        } else {
+            fileType = "storyfile"
+        }
+        const body = new FormData();
+        body.append('username', username);
+        body.append('password', password);
+        body.append('lastversion', xml.ifindex.story.ifdb.pageversion);
+        const ifiction = `<?xml version="1.0" encoding="UTF-8"?>
+        <ifindex version="1.0" xmlns="http://babel.ifarchive.org/protocol/iFiction/">
+        <story>
+        <identification><tuid>${tuid}</tuid></identification>
+        </story>
+        </ifindex>
+        `
+        body.append('ifiction', new Blob([ifiction], { type: 'text/xml' }));
+        const links = `<?xml version="1.0" encoding="UTF-8"?>
+        <downloads xmlns="http://ifdb.org/api/xmlns"><links>
+        <link>
+        <url>${downloadLink}</url>
+        <title>${downloadTitle}</title>
+        <isGame/>
+        <format>${fileType}</format>
+        ${fileType === 'executable' ? `<os>Windows.</os>`: ''}
+        <compression>zip</compression>
+        ${zipMainFile ? `<compressedPrimary>${zipMainFile}</compressedPrimary>`: ''}
+        </link>
+        </links></downloads>
+        `;
+        body.append('links', new Blob([links], { type: 'text/xml' }));
+        response = await fetch(`${url}/putific`, {
+            method: 'post',
+            body,
+        });
+        const { status, statusText, ok } = response;
+        const text = await response.text();
+        if (!ok) {
+            throw new Error(`Failed ${status} ${statusText} adding download link for ${name}: ${text}`);
+        }
+        console.log(name, `${url}/viewgame?id=${tuid}`, "OK");
+    })
 }
+
+for await (const result of runTasks(10, tasks.values())) { }
