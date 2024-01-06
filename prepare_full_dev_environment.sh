a4737ab7cb2163647498ffb1df735e3f20ad9690
Dan Fabulich <dan@fabulich.com>
2024-01-05 16:44:49 -0800

    Don't delete DEFINER, just set it to the `ifdb` user
    mysqldump likes to generate `DEFINER=` directives pointing to `root`. In the past, we could just drop any line containing `DEFINER=`, but now that we're using triggers, mysqldump likes to generate those smack dab in the middle of `CREATE TRIGGER` statements, so we need to keep them.
    
    Instead of deleting them, we're now setting `DEFINER=` to `ifdb`@`%`, which restores cleanly.

--- prepare_full_dev_environment.sh
+++ prepare_full_dev_environment.sh
@@ -5,7 +5,7 @@ IFDB_FULL_EXPORT=$1
 rm -rf initdb
 mkdir initdb
 echo "create database if not exists ifdb CHARACTER SET latin1 COLLATE latin1_german2_ci; use ifdb;" > initdb/ifdb.sql
-grep -a -v 'DEFINER=' $IFDB_FULL_EXPORT/ifdb-*.sql >> initdb/ifdb.sql
+perl -pe 's/DEFINER=`root`@`localhost`/DEFINER=`ifdb`@`%`/g' $IFDB_FULL_EXPORT/ifdb-*.sql >> initdb/ifdb.sql
 echo "create database if not exists ifdb_images0; use ifdb_images0;" | cat - $IFDB_FULL_EXPORT/ifdb_images0-*.sql > initdb/ifdb_images0.sql
 echo "create database if not exists ifdb_images1; use ifdb_images1;" | cat - $IFDB_FULL_EXPORT/ifdb_images1-*.sql > initdb/ifdb_images1.sql
 echo "create database if not exists ifdb_images2; use ifdb_images2;" | cat - $IFDB_FULL_EXPORT/ifdb_images2-*.sql > initdb/ifdb_images2.sql
