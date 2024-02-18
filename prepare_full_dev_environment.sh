3bb2a2ba661e6c2f3d2ff5e7db00bef5c720012b
Dan Fabulich <dan@fabulich.com>
2024-02-17 19:01:25 -0800

    Don't patch full environment, since prod is fully patched now

--- prepare_full_dev_environment.sh
+++ prepare_full_dev_environment.sh
@@ -11,7 +11,7 @@ echo "create database if not exists ifdb_images1; use ifdb_images1;" | cat - $IF
 echo "create database if not exists ifdb_images2; use ifdb_images2;" | cat - $IFDB_FULL_EXPORT/ifdb_images2-*.sql > initdb/ifdb_images2.sql
 echo "create database if not exists ifdb_images3; use ifdb_images3;" | cat - $IFDB_FULL_EXPORT/ifdb_images3-*.sql > initdb/ifdb_images3.sql
 echo "create database if not exists ifdb_images4; use ifdb_images4;" | cat - $IFDB_FULL_EXPORT/ifdb_images4-*.sql > initdb/ifdb_images4.sql
-cp sql/patch-schema-2.sql initdb/ifdb_patch_schema.sql
+#cp sql/patch-schema-2.sql initdb/ifdb_patch_schema.sql
 
 sed 's/"127.0.0.1", "username", "password"/"db", "root", "secret"/' local-credentials.php.template > www/local-credentials.php
 sed 's/return null/return localCredentials()/' www/local-credentials.php > www/local-credentials2.php
