bd439fe99868026ae076ab29d25eb47eac444f7d
Ori Avtalion <ori@avtalion.name>
2024-10-11 12:25:49 +0300

    WIP star buttons

--- www/starctl.php
+++ www/starctl.php
@@ -4,49 +4,20 @@ include_once "session-start.php";
 include_once "dbconnect.php";
 include_once "login-persist.php";
 
-$db = dbConnect();
 $curuser = checkPersistentLogin();
 
-function initStarControls()
+function showStarCtl($id)
 {
-    // Standard version - use the animated javascript star control,
-    // with automatic mouse rollover highlighting.
-    ?>
-    <script type="text/javascript" nonce="<?php global $nonce; echo $nonce; ?>">
-        function setStarCtlValue(id, value) {
-            if (value) {
-                document.getElementById(`${id}__rating${value}`).checked = true;
-            } else {
-                var checked = [...document.querySelectorAll(`#${id} input[type=radio]`)].filter(i => i.checked)[0];
-                if (checked) checked.checked = false;
-            }
-        }
-    </script>
-    <?php
-}
-
-function showStarCtl($id, $init, $clickFunc)
-{
+    global $nonce;
     
-    if (!$init)
-        $init = 0;
-
-    $str = "<fieldset id='$id' class='star-rating'><div>\n";
-    
+    $str = "<div id=\"$id\" class=\"star-container\" role=\"group\">";
+    // Button 0 is required for the CSS subsequent-sibling selectors to work.
+    $str .= "<button data-value=\"0\"></button>";
     for ($i = 1; $i <= 5; $i++) {
-        $checked = "";
-        if ($i == $init) {
-            $checked = "checked";
-        }
-        $str .= "<input type='radio' name='rating' value='$i' id='{$id}__rating$i' $checked>"
-            . "<label for='{$id}__rating$i'><span>$i</span></label>";
+        $str .= "<button data-value=\"$i\" role=\"button\" aria-label=\"Rate $i out of 5\">";
+        $str .= "</button>";
     }
-
-    $str .= addEventListener("change", "$clickFunc(event.target.value)");
-
-    $str .= "</div></fieldset>";
-
-    
+    $str .= "</div>";
 
     return $str;
 }
