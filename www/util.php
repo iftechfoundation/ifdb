d2f19d1bb2e16eb0c681ed0754639bd660ac4d37
Dan Fabulich <dan@fabulich.com>
2024-10-28 11:12:04 -0700

    Update links to coverart

--- www/util.php
+++ www/util.php
@@ -2953,13 +2953,13 @@ function check_admin_privileges($db, $userid) {
 }
 
 function coverArtThumbnail($id, $size, $params = "") {
-    $thumbnail = "/viewgame?id=$id&coverart&thumbnail=";
+    $thumbnail = "/coverart?id=$id&thumbnail=";
     $x15 = round($size * 3 / 2);
     $x2 = $size * 2;
     $x3 = $size * 3;
     global $nonce;
     return "<style nonce='$nonce'>.coverart__img { max-width: 35vw; height: auto; }</style>"
-        ."<img class='coverart__img' srcset=\"$thumbnail{$size}x$size$params, $thumbnail{$x15}x$x15$params 1.5x, $thumbnail{$x2}x$x2$params 2x, $thumbnail{$x3}x$x3$params 3x\" src=\"/viewgame?id=$id&coverart&thumbnail={$size}x$size\" height=$size width=$size border=0 alt=\"\">";
+        ."<img class='coverart__img' srcset=\"$thumbnail{$size}x$size$params, $thumbnail{$x15}x$x15$params 1.5x, $thumbnail{$x2}x$x2$params 2x, $thumbnail{$x3}x$x3$params 3x\" src=\"$thumbnail{$size}x$size$params\" height=$size width=$size border=0 alt=\"\">";
 }
 
 // ----------------------------------------------------------------------------
