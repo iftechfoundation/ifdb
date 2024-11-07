208e6f44ed52b6c9381cfec8bca0f33dd31903e8
alice-blue <alice-blue@users.noreply.github.com>
2024-11-06 22:21:33 -0500

    Patch-schema-2.sql: add playertimes table

--- sql/patch-schema-2.sql
+++ sql/patch-schema-2.sql
@@ -13,3 +13,55 @@ CREATE TABLE `blockedtagsynonyms` (
 
 insert into blockedtagsynonyms (blockedtag, preferredtag)
 values ('sci-fi', 'science fiction');
+
+
+--
+-- Table structure for table `playertimes`
+--
+
+DROP TABLE IF EXISTS `playertimes`;
+CREATE TABLE playertimes (
+  id INT AUTO_INCREMENT,
+  gameid VARCHAR(32) NOT NULL,
+  userid VARCHAR(32) NOT NULL,
+  timevote INT(5) unsigned not null,
+  PRIMARY KEY (id)
+);
+
+-- Sample time values for The Tempest (by Grigg)
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', 'kaw2cas7dyiq2tmg', 63);
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', '0000000000000000', 128);
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', '0000000000000001', 55);
+
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', 'pwamtkqtbeyc8eyn', 37);
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', '6cfekbbjqeduww77', 76);
+
+-- Sample time values for Ninja (by Panks)
+insert into playertimes (gameid, userid, timevote)
+values ('n93jonigjmva9e3g', 'kaw2cas7dyiq2tmg', 139);
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', '0000000000000001', 134);
+
+insert into playertimes (gameid, userid, timevote)
+values ('59g5czw7izz7aoip', '0000000000000000', 204);
+
+insert into playertimes (gameid, userid, timevote)
+values ('n93jonigjmva9e3g', 'pwamtkqtbeyc8eyn', 116);
+
+-- Sample time values for Four Seconds (by Reigstad)
+insert into playertimes (gameid, userid, timevote)
+values ('bu6mmul5vxci5vqc', 'kaw2cas7dyiq2tmg', 1);
+
+insert into playertimes (gameid, userid, timevote)
+values ('bu6mmul5vxci5vqc', 'pwamtkqtbeyc8eyn', 6);
+
