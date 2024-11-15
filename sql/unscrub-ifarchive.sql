07812d3e5b1af260a6a4d4327bd3fa970d2d835e
Dan Fabulich <dan@fabulich.com>
2024-11-15 11:38:05 -0800

    Unscrub UTF-8

--- sql/unscrub-ifarchive.sql
+++ sql/unscrub-ifarchive.sql
@@ -4,44 +4,44 @@ DROP TABLE IF EXISTS `audit`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `audit` (
-  `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+  `userid` varchar(32) NOT NULL DEFAULT '',
   `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `action` mediumtext COLLATE latin1_german2_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+  `action` mediumtext NOT NULL
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS `userfilters`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userfilters` (
-  `userid` char(32) COLLATE latin1_german2_ci NOT NULL,
-  `targetuserid` char(32) COLLATE latin1_german2_ci NOT NULL,
-  `filtertype` char(1) COLLATE latin1_german2_ci NOT NULL,
+  `userid` char(32) NOT NULL,
+  `targetuserid` char(32) NOT NULL,
+  `filtertype` char(1) NOT NULL,
   KEY `userid` (`userid`),
   KEY `targetuserid` (`targetuserid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 ALTER TABLE `users`
-    ADD COLUMN `email` varchar(255) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+    ADD COLUMN `email` varchar(255) NOT NULL DEFAULT '',
     ADD COLUMN `emailflags` tinyint(2) NOT NULL DEFAULT '3',
-    ADD COLUMN `profilestatus` varchar(1) COLLATE latin1_german2_ci DEFAULT NULL,
+    ADD COLUMN `profilestatus` varchar(1) DEFAULT NULL,
     ADD COLUMN `password` varchar(40) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-    ADD COLUMN `pswsalt` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-    ADD COLUMN `activationcode` varchar(40) COLLATE latin1_german2_ci DEFAULT NULL,
-    ADD COLUMN `acctstatus` char(1) COLLATE latin1_german2_ci NOT NULL DEFAULT 'A',
+    ADD COLUMN `pswsalt` varchar(32) NOT NULL DEFAULT '',
+    ADD COLUMN `activationcode` varchar(40) DEFAULT NULL,
+    ADD COLUMN `acctstatus` char(1) NOT NULL DEFAULT 'A',
     ADD COLUMN `lastlogin` datetime DEFAULT NULL,
-    ADD COLUMN `privileges` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '''''',
+    ADD COLUMN `privileges` varchar(32) NOT NULL DEFAULT '''''',
     ADD COLUMN `defaultos` int(11) unsigned DEFAULT NULL,
     ADD COLUMN `defaultosvsn` int(11) unsigned DEFAULT NULL,
     ADD COLUMN `noexedownloads` tinyint(1) NOT NULL DEFAULT '0',
-    ADD COLUMN `publiclists` varchar(10) COLLATE latin1_german2_ci DEFAULT NULL,
+    ADD COLUMN `publiclists` varchar(10) DEFAULT NULL,
     ADD COLUMN `mirrorid` int(11) unsigned NOT NULL DEFAULT '100',
     ADD COLUMN `stylesheetid` bigint(20) DEFAULT NULL,
-    ADD COLUMN `offsite_display` char(1) COLLATE latin1_german2_ci NOT NULL DEFAULT 'A',
+    ADD COLUMN `offsite_display` char(1) NOT NULL DEFAULT 'A',
     ADD COLUMN `accessibility` tinyint(1) DEFAULT NULL,
     ADD COLUMN `caughtupdate` datetime DEFAULT NULL,
-    ADD COLUMN `remarks` mediumtext COLLATE latin1_german2_ci,
+    ADD COLUMN `remarks` mediumtext,
     ADD COLUMN `tosversion` int(11) NOT NULL DEFAULT '1',
     ADD COLUMN `Sandbox` int(11) NOT NULL DEFAULT '0',
     ADD KEY `email` (`email`)
@@ -56,8 +56,8 @@ DROP TABLE IF EXISTS `formatprivs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `formatprivs` (
   `fmtid` int(11) unsigned NOT NULL DEFAULT '0',
-  `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+  `userid` varchar(32) NOT NULL DEFAULT ''
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,12 +68,12 @@ DROP TABLE IF EXISTS `logins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `logins` (
-  `uid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `ip` varchar(16) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+  `uid` varchar(32) NOT NULL DEFAULT '',
+  `ip` varchar(16) NOT NULL DEFAULT '',
   `when` datetime NOT NULL,
   KEY `uid` (`uid`),
   KEY `ip` (`ip`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,11 +84,11 @@ DROP TABLE IF EXISTS `nonces`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `nonces` (
-  `nonceid` varchar(256) COLLATE latin1_german2_ci NOT NULL,
-  `hash` varchar(40) COLLATE latin1_german2_ci NOT NULL,
+  `nonceid` varchar(256) NOT NULL,
+  `hash` varchar(40) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   KEY `nonceid` (`nonceid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,8 +100,8 @@ DROP TABLE IF EXISTS `osprivs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `osprivs` (
   `osid` int(11) unsigned NOT NULL DEFAULT '0',
-  `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+  `userid` varchar(32) NOT NULL DEFAULT ''
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,12 +112,12 @@ DROP TABLE IF EXISTS `persistentsessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `persistentsessions` (
-  `id` varchar(64) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+  `id` varchar(64) NOT NULL DEFAULT '',
+  `userid` varchar(32) NOT NULL DEFAULT '',
   `lastlogin` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,9 +128,9 @@ DROP TABLE IF EXISTS `privileges`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `privileges` (
-  `code` char(1) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `name` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+  `code` char(1) NOT NULL DEFAULT '',
+  `name` varchar(32) NOT NULL DEFAULT ''
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -142,17 +142,17 @@ DROP TABLE IF EXISTS `reviewflags`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reviewflags` (
   `reviewid` bigint(20) NOT NULL,
-  `flagger` varchar(32) COLLATE latin1_german2_ci NOT NULL,
-  `flagtype` char(1) COLLATE latin1_german2_ci NOT NULL,
-  `notes` mediumtext COLLATE latin1_german2_ci,
+  `flagger` varchar(32) NOT NULL,
+  `flagtype` char(1) NOT NULL,
+  `notes` mediumtext,
   `created` datetime NOT NULL,
   KEY `reviewid` (`reviewid`),
   KEY `flagger` (`flagger`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 ALTER TABLE `reviewvotes`
-  ADD COLUMN `userid` char(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+  ADD COLUMN `userid` char(32) NOT NULL DEFAULT '',
   ADD KEY `userid` (`userid`)
 ;
 
@@ -164,17 +164,17 @@ DROP TABLE IF EXISTS `stylepics`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stylepics` (
-  `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `name` varchar(128) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `picture` varchar(64) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
-  `desc` mediumtext COLLATE latin1_german2_ci,
+  `userid` varchar(32) NOT NULL DEFAULT '',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `picture` varchar(64) NOT NULL DEFAULT '',
+  `desc` mediumtext,
   `picturebytes` int(11) DEFAULT NULL,
   PRIMARY KEY (`userid`,`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 ALTER TABLE `ucomments`
-  ADD COLUMN `private` varchar(32) COLLATE latin1_german2_ci DEFAULT NULL
+  ADD COLUMN `private` varchar(32) DEFAULT NULL
 ;
 
 --
@@ -201,12 +201,12 @@ DROP TABLE IF EXISTS `userfilters`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userfilters` (
-  `userid` char(32) COLLATE latin1_german2_ci NOT NULL,
-  `targetuserid` char(32) COLLATE latin1_german2_ci NOT NULL,
-  `filtertype` char(1) COLLATE latin1_german2_ci NOT NULL,
+  `userid` char(32) NOT NULL,
+  `targetuserid` char(32) NOT NULL,
+  `filtertype` char(1) NOT NULL,
   KEY `userid` (`userid`),
   KEY `targetuserid` (`targetuserid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+) ENGINE=MyISAM;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -846,7 +846,7 @@ where ifnull((now() > `reviews`.`embargodate`), 1)
 
 drop table if exists userScores_mv;
 create table userScores_mv (
-    `userid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+    `userid` varchar(32) NOT NULL DEFAULT '',
     `score` int unsigned,
     `rankingScore` int unsigned,
     `reviewCount` int unsigned,
@@ -864,7 +864,7 @@ unlock tables;
 
 drop table if exists gameRatingsSandbox0_mv;
 create table gameRatingsSandbox0_mv (
-  `gameid` varchar(32) COLLATE latin1_german2_ci NOT NULL DEFAULT '',
+  `gameid` varchar(32) NOT NULL DEFAULT '',
   `rated1` int unsigned,
   `rated2` int unsigned,
   `rated3` int unsigned,
@@ -895,7 +895,7 @@ DROP PROCEDURE IF EXISTS refresh_gameRatingsSandbox0_mv;
 DELIMITER $$
 
 CREATE PROCEDURE refresh_gameRatingsSandbox0_mv (
-    IN new_gameid varchar(32) COLLATE latin1_german2_ci
+    IN new_gameid varchar(32)
 )
 BEGIN
 select *
