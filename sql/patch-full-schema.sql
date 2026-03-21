6c93a5939d5ff41ce9e95163ff84d89cd7f6d7a3
Dan Fabulich <dan@fabulich.com>
2026-03-21 13:35:23 -0700

    add global_settings to full schema

--- sql/patch-full-schema.sql
+++ sql/patch-full-schema.sql
@@ -1,1 +1,12 @@
 USE ifdb;
+
+DROP TABLE IF EXISTS `global_settings`;
+CREATE TABLE `global_settings` (
+  `setting_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `setting_name` VARCHAR(255) NOT NULL,
+  `current_value` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (setting_id),
+  UNIQUE KEY `setting_name` (`setting_name`)
+);
+insert into global_settings (setting_name, current_value)
+values ('account approval', 'normal');
